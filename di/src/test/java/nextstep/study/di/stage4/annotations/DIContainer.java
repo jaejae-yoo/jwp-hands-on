c7e08b9d53cb8606a96d8605dfb561e33d64ede3
jaejae-yoo <wotj102@gmail.com>
2022-09-27 23:55:44 +0900

    feat: di step4 테스트

--- di/src/test/java/nextstep/study/di/stage4/annotations/DIContainer.java
+++ di/src/test/java/nextstep/study/di/stage4/annotations/DIContainer.java
@@ -1,6 +1,12 @@
 package nextstep.study.di.stage4.annotations;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashSet;
+import java.util.NoSuchElementException;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
@@ -10,15 +16,64 @@ class DIContainer {
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        this.beans = initBeans(classes);
+        this.beans.forEach(this::initFields);
+    }
+
+    private Set<Object> initBeans(final Set<Class<?>> classes) {
+        Set<Object> beans = new HashSet<>();
+        for (Class<?> clazz : classes) {
+            try {
+                final Constructor<?> constructor = clazz.getDeclaredConstructor();
+                constructor.setAccessible(true);
+                beans.add(constructor.newInstance());
+            } catch (NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return beans;
+    }
+
+    private void initFields(final Object bean) {
+        final Field[] fields = bean.getClass()
+                .getDeclaredFields();
+        for (Field field : fields) {
+            field.setAccessible(true);
+            final Class<?> fieldType = field.getType();
+            initField(bean, field, fieldType);
+        }
+    }
+
+    private void initField(final Object targetBean, final Field field, final Class<?> fieldType) {
+        for (Object bean : beans) {
+            if (fieldType.isAssignableFrom(bean.getClass())) {
+                try {
+                    field.set(targetBean, bean);
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
 
     public static DIContainer createContainerForPackage(final String rootPackageName) {
-        return null;
+        final Set<Class<?>> classes = ClassPathScanner.getAllClassesInPackage(rootPackageName)
+                .stream()
+                .filter(DIContainer::filterAnnotation)
+                .collect(Collectors.toSet());
+
+        return new DIContainer(classes);
+    }
+
+    private static boolean filterAnnotation(final Class<?> clazz) {
+        return clazz.isAnnotationPresent(Service.class) || clazz.isAnnotationPresent(Repository.class);
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        return (T) beans.stream()
+                .filter(bean -> aClass.isAssignableFrom(bean.getClass()))
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException("빈이 존재하지 않습니다."));
     }
 }
