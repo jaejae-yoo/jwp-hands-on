4849ccfd88fe765cd16e6414671f110a588d09a8
jaejae-yoo <wotj102@gmail.com>
2022-10-16 21:54:07 +0900

    test: stage0 작성

--- aop/src/test/java/aop/stage0/TransactionHandler.java
+++ aop/src/test/java/aop/stage0/TransactionHandler.java
@@ -1,15 +1,35 @@
 package aop.stage0;
 
+import aop.DataAccessException;
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 public class TransactionHandler implements InvocationHandler {
+
+    private final PlatformTransactionManager transactionManager;
+    private final Object target;
+
+    public TransactionHandler(final PlatformTransactionManager transactionManager, final Object target) {
+        this.transactionManager = transactionManager;
+        this.target = target;
+    }
 
     /**
      * @Transactional 어노테이션이 존재하는 메서드만 트랜잭션 기능을 적용하도록 만들어보자.
      */
     @Override
-    public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable {
-        return null;
+    public Object invoke(final Object proxy, final Method method, final Object[] args) {
+        final var transactionStatus = transactionManager.getTransaction(new DefaultTransactionDefinition());
+        try {
+            final var result = method.invoke(target, args);
+            transactionManager.commit(transactionStatus);
+            return result;
+        } catch (InvocationTargetException | IllegalAccessException e) {
+            transactionManager.rollback(transactionStatus);
+            throw new DataAccessException(e);
+        }
     }
 }
